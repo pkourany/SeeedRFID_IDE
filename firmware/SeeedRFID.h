7aa310f88d7d40ead7a055904a886782c76e49dc
Harrison Jones <harrisonhjones@users.noreply.github.com>
2015-12-09 21:54:23 -0800

    Updated to support P1 or Electron

--- firmware/SeeedRFID.h
+++ firmware/SeeedRFID.h
@@ -75,7 +75,7 @@ enum RFIDType
 class SeeedRFID
 {
 private:
-#if (PLATFORM_ID == 0) || (PLATFORM_ID == 6)	//Core or Photon
+#if (PLATFORM_ID == 0) || (PLATFORM_ID == 6) || (PLATFORM_ID == 8) || (PLATFORM_ID == 10)	// Core or Photon or P1 or Electron
 	USARTSerial * _rfidIO; // software serial
 #else
 	SoftwareSerial * _rfidIO; // software serial
