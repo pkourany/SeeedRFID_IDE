8ed2f759e5393c31d50d80e5bd60a7bc565b1c62
Harrison Jones <harrisonhjones@users.noreply.github.com>
2015-12-09 21:44:40 -0800

    Updated to support P1 or Electron

--- firmware/SeeedRFID.h
+++ firmware/SeeedRFID.h
@@ -51,7 +51,7 @@
 #ifndef SeeedRFID_H
 #define SeeedRFID_H
 
-#if (PLATFORM_ID == 0) || (PLATFORM_ID == 6)	//Core or Photon
+#if (PLATFORM_ID == 0) || (PLATFORM_ID == 6) || (PLATFORM_ID == 8) || (PLATFORM_ID == 10)	// Core or Photon or P1 or Electron
 #include "application.h"
 #else
 #include <SoftwareSerial.h>
